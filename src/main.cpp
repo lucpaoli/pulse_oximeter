bfc92473edbd505cd44521b9cead58eb665cbd03
luc paoli <39316550+lucpaoli@users.noreply.github.com>
2022-11-27 02:09:01 -0500

    abstracted to function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <threshold.h>
+#include <shaggy.h>
 
 const int GREEN_GPIO = 27;
 const int RED_GPIO = 14;
@@ -8,42 +8,31 @@ const int SENSOR_GPIO = 25;
 const int GREEN_CHANNEL = 5;
 const int RED_CHANNEL = 0;
 const int RESOLUTION = 8;
-const int FREQ = 1000;           // Frequency of pulsing in Hz
-const int RED_DUTY_CYCLE = 255;  // for 50% duty cycle (0 is off, 255 is on)
+const int FREQ = 1000;            // Frequency of pulsing in Hz
+const int RED_DUTY_CYCLE = 255;   // for 50% duty cycle (0 is off, 255 is on)
 const int GREEN_DUTY_CYCLE = 255; // for 50% duty cycle (0 is off, 255 is on)
 
 // const unsigned long light_switch_interval = 2500;
 
-// int red_data;
-// int green_data;
 unsigned long current_ms;
 // unsigned long prev_ms = 0;
 // boolean is_red = false;
 
-int min_val = INT_MAX; // minimum value read from sensor
-int max_val = INT_MIN; // maximum value read from sensor
-unsigned long t_max_cur;
-unsigned long t_max_pre = 0;
-unsigned long t_min_cur;
-unsigned long t_min_pre = 0;
-double t_pulse;
-
 // vector to store sensor data
-int PPG_sample;
-int PPG_sample_filtered;
-const float alpha_PPG = 0.1; // tunable const in lowpass filter
+// int PPG_sample;
+// int PPG_sample_filtered;
+double ACDC_green, ACDC_red;
+const float alpha_PPG = 0.1;  // tunable const in leaky integrator
+const float alpha_ACDC = 0.2; // tunable const in leaky integrator
 
-bool last_extrema_was_max = false;
-bool last_extrema_was_min = true;
+// bool last_extrema_was_max = false;
+// bool last_extrema_was_min = true;
+
 // const int PPG_average_size = sizeof(PPG_sample) - PPG_window_size;
 
-float threshold = 400;
-float HR = 0;
-float HR_filtered = 0;
-float HR_printed = 0;
-const float alpha_HR = 0.1; // tunable const in lowpass filter
-
-int i = 0;
+double HR_filtered = 0;
+float R;
+const float alpha_HR = 0.5; // tunable const in leaky integrator
 
 void setup()
 {
@@ -63,8 +52,6 @@ void setup()
 
   // ledcWrite(RED_CHANNEL, RED_DUTY_CYCLE);
   // ledcWrite(GREEN_CHANNEL, 0);
-  ledcWrite(GREEN_CHANNEL, RED_DUTY_CYCLE);
-  ledcWrite(RED_CHANNEL, 0);
 
   // Configure serial monitor
   Serial.begin(115200);
@@ -73,83 +60,36 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  // read sensor data into PPG_sample
-  PPG_sample = analogRead(SENSOR_GPIO);
+  // ledcWrite(GREEN_CHANNEL, GREEN_DUTY_CYCLE);
+  // ledcWrite(RED_CHANNEL, 0);
+  // ReadACDC(&ACDC_green, &HR_filtered, SENSOR_GPIO, alpha_PPG, alpha_HR, alpha_ACDC);
 
-  PPG_sample_filtered = PPG_sample_filtered*alpha_PPG + (1-alpha_PPG)*PPG_sample;
-  // apply moving average
-  // sum = sum + PPG_sample_vec[idx];
-  // idx = (idx + 1) % window_size;
+  ledcWrite(RED_CHANNEL, RED_DUTY_CYCLE);
+  ledcWrite(GREEN_CHANNEL, 0);
+  ReadACDC(&ACDC_red, &HR_filtered, SENSOR_GPIO, alpha_PPG, alpha_HR, alpha_ACDC);
 
-  // PPG_sample = sum / window_size;
+  // Serial.print("HR:" + String(HR_filtered));
+  // Serial.print(", ");
+  // Serial.print("ACDC_green:" + String(ACDC_green));
+  // Serial.print(", ");
+  // Serial.println("ACDC_red:" + String(ACDC_red));
 
-  // search for local max & min
-  if (PPG_sample_filtered > max_val)
-  {
-    max_val = PPG_sample_filtered;
-    t_max_cur = millis();
-  }
-  if (PPG_sample_filtered < min_val)
-  {
-    min_val = PPG_sample_filtered;
-    t_min_cur = millis();
-  }
+  // R = ACDC_red/ACDC_green;
 
-  // check if current time exceeds threshold
-  current_ms = millis();
-  if (((current_ms - t_max_cur) > threshold) && (last_extrema_was_min))
-  {
-    // register max as local max
-    last_extrema_was_max = true;
-    last_extrema_was_min = false;
-
-    // TODO calculate HR
-    t_pulse = t_max_cur - t_max_pre;
-    t_max_pre = t_max_cur;
-
-    if (t_pulse > 0)
-    {
-      HR = 60000 / t_pulse;
-    }
-    else
-    {
-      HR = 0;
-    }
-
-    HR_filtered = HR*alpha_HR + HR_filtered*(1-alpha_HR);
-    max_val = INT_MIN;
-  }
-  if (((current_ms - t_min_cur) > threshold) && (last_extrema_was_max))
-  {
-    // register min as local min
-    last_extrema_was_max = false;
-    last_extrema_was_min = true;
-    t_min_pre = t_min_cur;
-    
-    // TODO calculate ACred, DCred, ACir, DCir, R
-    // TODO Calculate SpO2
-    min_val = INT_MAX;
-  }
-
-  
-  // if (i > 500) {
-  //   Serial.println("PPG_sample:" + String(PPG_sample));
-  //   i = 0;
+  //* Debug printing, ignore this lol
+  // if (HR_filtered != HR_printed)
+  // {
+  //   Serial.print("HR:" + String(HR_filtered));
+  //   Serial.print(", ");
+  //   Serial.println("t_pulse:" + String(t_pulse));
+  //   HR_printed = HR_filtered;
   // }
-  // i += 1;
-  if (HR_filtered != HR_printed) {
-    Serial.print("HR:" + String(HR_filtered));
-    Serial.print(", ");
-    Serial.println("t_pulse:" + String(t_pulse));
-    HR_printed = HR_filtered;
-  }
   // Serial.print("HR:" + String(HR_filtered));
   // Serial.print(", ");
   // Serial.println("t_pulse:" + String(t_pulse));
 
   // TODO Compute mean values from last 8 estimations
   // TODO Select new threshold from LUT based on HR
-  threshold = threshold_from_HR(HR_filtered);
 
   // // Change the color every second (see with interval variable)
   // if (current_ms - prev_ms >= light_switch_interval)
@@ -178,4 +118,3 @@ void loop()
   //   Serial.println(green_data);
   // }
 }
-
---
+++ src/shaggy.h
@@ -0,0 +1,82 @@
+#include <limits.h>
+#include <threshold.h>
+#include <Arduino.h>
+
+void ReadACDC(double *ACDC_filtered_ptr, double *HR_filtered_ptr, int SENSOR_GPIO, float alpha_PPG, float alpha_HR, float alpha_ACDC)
+{
+    int PPG_min_pre, PPG_max_pre, PPG_min_cur = INT_MAX, PPG_max_cur = INT_MIN;
+    unsigned long current_ms, t_max_cur, t_max_pre = 0, t_min_cur, t_min_pre = 0, t_pulse;
+
+    float PPG_sample_filtered, d, f, AC, DC, HR, threshold = threshold_from_HR(*HR_filtered_ptr);
+    // float PPG_sample_filtered, d, f, AC, DC, HR, threshold = 340;
+    int PPG_sample;
+    bool last_extrema_was_max = false, last_extrema_was_min = true;
+
+    int t1 = millis();
+    while ((millis() - t1) < 10000)
+    {
+        // read sensor data into PPG_sample
+        PPG_sample = analogRead(SENSOR_GPIO);
+
+        PPG_sample_filtered = PPG_sample_filtered * alpha_PPG + (1 - alpha_PPG) * PPG_sample;
+
+        // search for local max & min
+        if (PPG_sample_filtered > PPG_max_cur)
+        {
+            PPG_max_cur = PPG_sample_filtered;
+            t_max_cur = millis();
+        }
+        if (PPG_sample_filtered < PPG_min_cur)
+        {
+            // min_val =
+            PPG_min_cur = PPG_sample_filtered;
+            t_min_cur = millis();
+        }
+
+        // check if current time exceeds threshold
+        current_ms = millis();
+        if (((current_ms - t_max_cur) > threshold) && (last_extrema_was_min))
+        {
+            // register max as local max
+            last_extrema_was_max = true;
+            last_extrema_was_min = false;
+
+            // Calculate HR
+            t_pulse = t_max_cur - t_max_pre;
+            t_max_pre = t_max_cur;
+
+            if (t_pulse > 0)
+            {
+                HR = 60000 / t_pulse;
+                // Leaky integrator
+                *HR_filtered_ptr = HR * alpha_HR + *HR_filtered_ptr * (1 - alpha_HR);
+
+                threshold = threshold_from_HR(*HR_filtered_ptr);
+                Serial.println("HR:" + String(*HR_filtered_ptr));
+            }
+
+            PPG_max_pre = PPG_max_cur;
+            PPG_max_cur = INT_MIN;
+        }
+        if (((current_ms - t_min_cur) > threshold) && (last_extrema_was_max))
+        {
+            // register min as local min
+            last_extrema_was_max = false;
+            last_extrema_was_min = true;
+
+            // calculate AC DC ratio
+            // if (t_min_pre > 1)
+            // {
+            //     d = (PPG_min_cur - PPG_min_pre) / (t_min_cur - t_min_pre); // slope
+            //     f = PPG_min_cur - d * t_min_cur;                           // y-intercept
+            //     DC = d * t_max_cur + f;                                    // y = mx + f
+            //     AC = PPG_max_cur - DC;
+            //     *ACDC_filtered_ptr = *ACDC_filtered_ptr * alpha_ACDC + AC / DC * (1 - alpha_ACDC); // Leaky integrator
+            // }
+
+            t_min_pre = t_min_cur;
+            PPG_min_pre = PPG_min_cur;
+            PPG_min_cur = INT_MAX;
+        }
+    }
+}
