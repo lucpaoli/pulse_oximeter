58881a1a768a18c7fe2345443ef0dd96c71c53cb
luc paoli <39316550+lucpaoli@users.noreply.github.com>
2022-11-26 21:52:12 -0500

    probably working pulse detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <math.h>
+#include <threshold.h>
 
 const int GREEN_GPIO = 27;
 const int RED_GPIO = 14;
@@ -10,23 +10,40 @@ const int RED_CHANNEL = 0;
 const int RESOLUTION = 8;
 const int FREQ = 1000;           // Frequency of pulsing in Hz
 const int RED_DUTY_CYCLE = 255;  // for 50% duty cycle (0 is off, 255 is on)
-const int GREEN_DUTY_CYCLE = 70; // for 50% duty cycle (0 is off, 255 is on)
+const int GREEN_DUTY_CYCLE = 255; // for 50% duty cycle (0 is off, 255 is on)
 
-const unsigned long light_switch_interval = 2500;
+// const unsigned long light_switch_interval = 2500;
 
-int red_data;
-int green_data;
+// int red_data;
+// int green_data;
 unsigned long current_ms;
-unsigned long prev_ms = 0;
-boolean is_red = false;
+// unsigned long prev_ms = 0;
+// boolean is_red = false;
 
-// int min_val; // minimum value read from sensor
-// int max_val; // maximum value read from sensor
+int min_val = INT_MAX; // minimum value read from sensor
+int max_val = INT_MIN; // maximum value read from sensor
+unsigned long t_max_cur;
+unsigned long t_max_pre = 0;
+unsigned long t_min_cur;
+unsigned long t_min_pre = 0;
+double t_pulse;
 
 // vector to store sensor data
-int PPG_sample[100];
-const int PPG_window_size = 10;
-const int PPG_average_size = sizeof(PPG_sample) - PPG_window_size;
+int PPG_sample;
+int PPG_sample_filtered;
+const float alpha_PPG = 0.1; // tunable const in lowpass filter
+
+bool last_extrema_was_max = false;
+bool last_extrema_was_min = true;
+// const int PPG_average_size = sizeof(PPG_sample) - PPG_window_size;
+
+float threshold = 400;
+float HR = 0;
+float HR_filtered = 0;
+float HR_printed = 0;
+const float alpha_HR = 0.1; // tunable const in lowpass filter
+
+int i = 0;
 
 void setup()
 {
@@ -44,6 +61,11 @@ void setup()
   ledcAttachPin(GREEN_GPIO, GREEN_CHANNEL);
   ledcAttachPin(RED_GPIO, RED_CHANNEL);
 
+  // ledcWrite(RED_CHANNEL, RED_DUTY_CYCLE);
+  // ledcWrite(GREEN_CHANNEL, 0);
+  ledcWrite(GREEN_CHANNEL, RED_DUTY_CYCLE);
+  ledcWrite(RED_CHANNEL, 0);
+
   // Configure serial monitor
   Serial.begin(115200);
 }
@@ -51,83 +73,109 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  current_ms = millis();
-
   // read sensor data into PPG_sample
-  for (int i = 0; i < 100; i++)
-  {
-    PPG_sample[i] = analogRead(SENSOR_GPIO);
-  }
-  // calculate moving average of PPG_sample with a window size of PPG_sample_size
-  int moving_avg[PPG_average_size];
-  for (int i = 0; i < PPG_average_size; i++)
-  {
-    moving_avg[i] = 0;
-    for (int j = 0; j < PPG_window_size; j++)
-    {
-      moving_avg[i] += PPG_sample[i + j];
-    }
-    moving_avg[i] /= PPG_window_size;
-  }
+  PPG_sample = analogRead(SENSOR_GPIO);
+
+  PPG_sample_filtered = PPG_sample_filtered*alpha_PPG + (1-alpha_PPG)*PPG_sample;
+  // apply moving average
+  // sum = sum + PPG_sample_vec[idx];
+  // idx = (idx + 1) % window_size;
+
+  // PPG_sample = sum / window_size;
 
   // search for local max & min
-  int max = -32768;
-  int min = 32767;
-  for (int i = 0; i < PPG_average_size; i++)
+  if (PPG_sample_filtered > max_val)
   {
-    if (moving_avg[i] > max)
+    max_val = PPG_sample_filtered;
+    t_max_cur = millis();
+  }
+  if (PPG_sample_filtered < min_val)
+  {
+    min_val = PPG_sample_filtered;
+    t_min_cur = millis();
+  }
+
+  // check if current time exceeds threshold
+  current_ms = millis();
+  if (((current_ms - t_max_cur) > threshold) && (last_extrema_was_min))
+  {
+    // register max as local max
+    last_extrema_was_max = true;
+    last_extrema_was_min = false;
+
+    // TODO calculate HR
+    t_pulse = t_max_cur - t_max_pre;
+    t_max_pre = t_max_cur;
+
+    if (t_pulse > 0)
     {
-      max = moving_avg[i];
-      // write time_max
-      //! what is time_max?! defined as current_timeslot
+      HR = 60000 / t_pulse;
     }
-    if (moving_avg[i] < min)
+    else
     {
-      min = moving_avg[i];
-      // write time_min
+      HR = 0;
     }
+
+    HR_filtered = HR*alpha_HR + HR_filtered*(1-alpha_HR);
+    max_val = INT_MIN;
+  }
+  if (((current_ms - t_min_cur) > threshold) && (last_extrema_was_max))
+  {
+    // register min as local min
+    last_extrema_was_max = false;
+    last_extrema_was_min = true;
+    t_min_pre = t_min_cur;
+    
+    // TODO calculate ACred, DCred, ACir, DCir, R
+    // TODO Calculate SpO2
+    min_val = INT_MAX;
   }
 
-  // Change the color every second (see with interval variable)
-  if (current_ms - prev_ms >= light_switch_interval)
-  {
-    is_red = !is_red;
-    prev_ms = current_ms;
+  
+  // if (i > 500) {
+  //   Serial.println("PPG_sample:" + String(PPG_sample));
+  //   i = 0;
+  // }
+  // i += 1;
+  if (HR_filtered != HR_printed) {
+    Serial.print("HR:" + String(HR_filtered));
+    Serial.print(", ");
+    Serial.println("t_pulse:" + String(t_pulse));
+    HR_printed = HR_filtered;
   }
+  // Serial.print("HR:" + String(HR_filtered));
+  // Serial.print(", ");
+  // Serial.println("t_pulse:" + String(t_pulse));
 
-  // Red LED pulse
-  if (is_red)
-  {
-    while
-      ledcWrite(RED_CHANNEL, RED_DUTY_CYCLE);
-    ledcWrite(GREEN_CHANNEL, 0);
-    red_data = analogRead(SENSOR_GPIO);
-    Serial.print("red:");
-    Serial.println(red_data);
-  }
+  // TODO Compute mean values from last 8 estimations
+  // TODO Select new threshold from LUT based on HR
+  threshold = threshold_from_HR(HR_filtered);
 
-  // Green LED pulse
-  else
-  {
-    ledcWrite(RED_CHANNEL, 0);
-    ledcWrite(GREEN_CHANNEL, GREEN_DUTY_CYCLE);
-    green_data = analogRead(SENSOR_GPIO);
-    Serial.print("green:");
-    Serial.println(green_data);
-  }
+  // // Change the color every second (see with interval variable)
+  // if (current_ms - prev_ms >= light_switch_interval)
+  // {
+  //   is_red = !is_red;
+  //   prev_ms = current_ms;
+  // }
+
+  // // Red LED pulse
+  // if (is_red)
+  // {
+  //   ledcWrite(RED_CHANNEL, RED_DUTY_CYCLE);
+  //   ledcWrite(GREEN_CHANNEL, 0);
+  //   red_data = analogRead(SENSOR_GPIO);
+  //   Serial.print("red:");
+  //   Serial.println(red_data);
+  // }
+
+  // // Green LED pulse
+  // else
+  // {
+  //   ledcWrite(RED_CHANNEL, 0);
+  //   ledcWrite(GREEN_CHANNEL, GREEN_DUTY_CYCLE);
+  //   green_data = analogRead(SENSOR_GPIO);
+  //   Serial.print("green:");
+  //   Serial.println(green_data);
+  // }
 }
 
-// // function that returns a moving average of the PPG_sample vector
-// void moving_average(int PPG_sample[], int PPG_sample_size, int PPG_window_size)
-// {
-//   int moving_avg[PPG_sample_size - PPG_window_size];
-//   for (int i = 0; i < PPG_sample_size - PPG_window_size; i++)
-//   {
-//     moving_avg[i] = 0;
-//     for (int j = 0; j < PPG_window_size; j++)
-//     {
-//       moving_avg[i] += PPG_sample[i + j];
-//     }
-//     moving_avg[i] /= PPG_window_size;
-//   }
-// }
---
+++ src/threshold.h
@@ -0,0 +1,37 @@
+// return the threshold value in ms for the given HR
+// table 2 from 10.1109/MOCAST52088.2021.9493400
+float threshold_from_HR(float HR)
+{
+  if (HR < 60)
+  {
+    return 400;
+  }
+  else if (HR < 70)
+  {
+    return 360;
+  }
+  else if (HR < 80)
+  {
+    return 320;
+  }
+  else if (HR < 100)
+  {
+    return 280;
+  }
+  else if (HR < 120)
+  {
+    return 250;
+  }
+  else if (HR < 140)
+  {
+    return 220;
+  }
+  else if (HR < 160)
+  {
+    return 200;
+  }
+  else
+  {
+    return 180;
+  }
+}
